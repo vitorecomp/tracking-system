3e64db5451aa01e9f0c6aa6ba2d6b7966490b691
vitorecomp <vitor.ecomp@gmail.com>
2016-06-07 12:57:53 -0300

    arrumando indent

--- tracking-database-gen/source/main.cpp
+++ tracking-database-gen/source/main.cpp
@@ -1,96 +1,89 @@
-#include <iostream>
-#include <stdlib.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <iostream>
 
-//files
+// files
 #include "./header/Tracker.hpp"
-
 #include "./libs/output/IOMananger.hpp"
 
-//defines
+// defines
 #define ESC 27
 
-//namespaces
+// namespaces
 using namespace std;
 
-
-//main
+// main
 int main(int argc, char *argv[]) {
-	stringstream msg;
+	// verify input
+	if (argc < 2) {
+		io::configs.runFile("{}");
+	} else {
+		io::configs.openFile(argv[1]);
+	}
 
-    //io
-    if(argc < 2){
-        cout << "Por favor, entrar com os argumentos corretamente." << endl;
-        cout << "./tracking_system configFile.json" << endl;
-        cout << endl;
-        cout << "configFile.json sendo um arquivo que contenha as configurações necessarias" << endl;
-        cout << "para executar o programa" << endl;
-        exit(1);
-    }
+	io::configs.setConfigs();
+	thread io_th(run_io);
 
-    //gera as configuracoes
-    io::configs.openFile(argv[1]);
-    io::configs.setConfigs();
-    thread io_th(run_io);
+	// cria os parametros iniciais
+	TrackerCore tracker_core;
 
-    //cria os parametros iniciais
-    TrackerCore tracker_core;
+	// cria populacoes
+	tracker_core.startTrackers();
 
-    //cria populacoes
-    tracker_core.startTrackers();
+	// abrir videos
+	VideoWriter output_video;
 
-    //abrir videos
-    VideoWriter output_video;
+	// get parameters
+	string video_name = tracker_core.getVideoName();
+	uint fps = tracker_core.getFrameRate();
+	Size size = tracker_core.getVideoSize();
+	static int codec = CV_FOURCC('M', 'J', 'P', 'G');
 
-    //get parameters
-    string video_name = tracker_core.getVideoName();
-    uint fps  = tracker_core.getFrameRate();
-    Size size = tracker_core.getVideoSize();
-	static int codec = CV_FOURCC('M','J','P','G');
+	// generate video
+	output_video.open(video_name, codec, fps, size, true);
 
-    //generate video
-    output_video.open(video_name, codec, fps, size, true);
-	if (!output_video.isOpened()){
-
-		msg << "Could not open the output video for write: " << video_name << endl;
-		io::output.printMsgBox("main_messages", msg.str());
-		msg.str("");
-
-  		io::input.endSignal();
-	  	io_th.join();
-      	return 0;
-  	}
+	if (!output_video.isOpened()) {
+		io::man.out_log(Msg()) << "Could not open the output video for write: "
+							   << video_name;
+		io::input.endSignal();
+		io_th.join();
+		return 0;
+	}
 
 	uint i = 0;
-    //sumariza informações
-    while(tracker_core.hasFrames()){
+	// sumariza informações
+	while (tracker_core.hasFrames()) {
+		// gera as novas posiçoes
+		tracker_core.generatePositions();
 
-        //gera as novas posiçoes
-        tracker_core.generatePositions();
+		// desenha
+		tracker_core.draw();
 
-        //desenha
-        tracker_core.draw();
-
-        //pega o frame
-        Mat frame = tracker_core.getFrame();
+		// pega o frame
+		Mat frame = tracker_core.getFrame();
 
 		msg << "Frame processo :" << i++;
 		io::output.printMsgBox("main_messages", msg.str());
 		msg.str("");
-        //joga no videos
-		//imshow("Teste", frame);
-		//uint key = waitKey(10);
-		//if(key == ESC)
-		//	break;
-        output_video << frame;
-    }
 
-	//generate json
+		// joga no videos
+		if (io.configs.debug == true) {
+			imshow("Teste", frame);
+			uint key = waitKey(10);
+			if (key == ESC)
+				break;
+		}
+
+		output_video << frame;
+	}
+
+	// generate json
 	tracker_core.generateGroundTruth();
 
-    //salva a info
-    tracker_core.saveDatabaseJson();
+	// salva a info
+	tracker_core.saveDatabaseJson();
 	io::input.endSignal();
 	io_th.join();
-    return 0;
+
+	return 0;
 }
--- tracking-database-gen/source/src/Tracker.cpp
+++ tracking-database-gen/source/src/Tracker.cpp
@@ -1,13 +1,13 @@
 #include "../header/Tracker.hpp"
 
-TrackerCore::TrackerCore(){
+TrackerCore::TrackerCore() {
 	srand(time(NULL));
 
 
 	this->x_res = io::configs.x_resolution;
 	this->y_res = io::configs.y_resolution;
 
-	if(io::configs.colored)
+	if (io::configs.colored)
 		this->frame = Mat::zeros(this->x_res, this->y_res, CV_8UC3);
 	else
 		this->frame = Mat::zeros(this->x_res, this->y_res, CV_8UC1);
@@ -18,103 +18,104 @@ TrackerCore::TrackerCore(){
 }
 
 
-void TrackerCore::startTrackers(){
-	for(uint i = 0; i < this->num_trackers; i++){
+void TrackerCore::startTrackers() {
+	for (uint i = 0; i < this->num_trackers; i++) {
 		trackers.push_back(Tracker(this->frame));
 	}
 }
 
-std::string TrackerCore::getVideoName(){
+std::string TrackerCore::getVideoName() {
 	return io::configs.output_video_name;
 }
 
-uint TrackerCore::getFrameRate(){
+uint TrackerCore::getFrameRate() {
 	return io::configs.video_frame_rate;
 }
 
-Size TrackerCore::getVideoSize(){
+Size TrackerCore::getVideoSize() {
 	return Size(this->y_res, this->x_res);
 }
 
-void TrackerCore::generatePositions(){
-	for(uint i = 0; i < this->num_trackers; i++){
+void TrackerCore::generatePositions() {
+	for (uint i = 0; i < this->num_trackers; i++) {
 		trackers[i].generatePosition();
 	}
 }
-void TrackerCore::draw(){
-	if(io::configs.colored)
+void TrackerCore::draw() {
+	if (io::configs.colored)
 		this->frame = Mat::zeros(this->x_res, this->y_res, CV_8UC3);
 	else
 		this->frame = Mat::zeros(this->x_res, this->y_res, CV_8UC1);
 
-	for(uint i = 0; i < this->num_trackers; i++){
+	for (uint i = 0; i < this->num_trackers; i++) {
 		trackers[i].draw();
 	}
 }
 
-Mat TrackerCore::getFrame(){
+Mat TrackerCore::getFrame() {
 	return this->frame;
 }
 
-bool TrackerCore::hasFrames(){
+bool TrackerCore::hasFrames() {
 	this->num_frames--;
 
-	if(num_frames <= 0)
+	if (num_frames <= 0)
 		return false;
 
 	return true;
 }
 
-void TrackerCore::saveDatabaseJson(){
-
+void TrackerCore::saveDatabaseJson() {
 }
 
-void TrackerCore::generateGroundTruth(){
+void TrackerCore::generateGroundTruth() {
 	uint num_frames = io::configs.video_frame_rate * io::configs.duration;
 	Json::Value json;
- 	Json::StyledWriter writer;
-	json["frames"] =  Json::Value(Json::arrayValue);
+	Json::StyledWriter writer;
+	json["frames"] = Json::Value(Json::arrayValue);
 
-	for(uint i = 0; i < num_frames; i++){
+	for (uint i = 0; i < num_frames; i++) {
 		Json::Value arr;
 		json["frames"].append(arr);
 		json["frames"][i]["frame"] = i;
 		json["frames"][i]["num_trackers"] = this->num_trackers;
 		json["frames"][i]["positions"] = Json::Value(Json::arrayValue);
-		for(uint j = 0; j < this->num_trackers; j++){
+		for (uint j = 0; j < this->num_trackers; j++) {
 			trackers[j].positions[i];
 			Json::Value posic;
 			json["frames"][i]["positions"].append(posic);
 			json["frames"][i]["positions"][j]["frame_id"] = trackers[j].id;
-			json["frames"][i]["positions"][j]["x_pos"] = trackers[j].positions[i].x;
-			json["frames"][i]["positions"][j]["y_pos"] = trackers[j].positions[i].y;
+			json["frames"][i]["positions"][j]["x_pos"] =
+				trackers[j].positions[i].x;
+			json["frames"][i]["positions"][j]["y_pos"] =
+				trackers[j].positions[i].y;
 		}
 	}
 
 
 	ofstream myfile;
-	myfile.open (io::configs.ground_truth_file);
+	myfile.open(io::configs.ground_truth_file);
 	myfile << writer.write(json);
 	myfile.close();
 }
 
 uint Tracker::to_id = 0;
 
-void Tracker::draw(){
+void Tracker::draw() {
 	positions.push_back(Point(x_position, y_position));
 
-	for(uint i = 0; i < image.cols; i++)
-		for(uint j = 0; j < image.rows; j++)
-			for(uint color = 0; color < 3; color++){
-				if(image.at<Vec3b>(i, j)[color] != 0)
-					frames.at<Vec3b>(
-						this->y_position + i,
-					 	this->x_position + j)[color] = image.at<Vec3b>(i, j)[color];
-				}
+	for (uint i = 0; i < image.cols; i++)
+		for (uint j = 0; j < image.rows; j++)
+			for (uint color = 0; color < 3; color++) {
+				if (image.at<Vec3b>(i, j)[color] != 0)
+					frames.at<Vec3b>(this->y_position + i,
+									 this->x_position + j)[color] =
+						image.at<Vec3b>(i, j)[color];
+			}
 }
 
-void Tracker::generatePosition(){
-	if(this->dead == true){
+void Tracker::generatePosition() {
+	if (this->dead == true) {
 		this->recreate();
 		return;
 	}
@@ -126,40 +127,38 @@ void Tracker::generatePosition(){
 
 
 	x_position = this->x_position;
-	uint max = rand()%io::configs.max_mov + 1;
-	uint moviment = rand()%max * pow(-1, rand()%2);
+	uint max = rand() % io::configs.max_mov + 1;
+	uint moviment = rand() % max * pow(-1, rand() % 2);
 	x_position += moviment;
-	if(!(x_position > size_x + 1 && x_position < max_x)){
-		if(io::configs.no_bounded)
+	if (!(x_position > size_x + 1 && x_position < max_x)) {
+		if (io::configs.no_bounded)
 			this->dead = true;
 		else
-			x_position += -2*moviment;
-
+			x_position += -2 * moviment;
 	}
 	this->x_position = x_position;
 
 
-
 	y_position = this->y_position;
-	max = rand()%io::configs.max_mov + 1;
-	moviment = rand()%max * pow(-1, rand()%2);
+	max = rand() % io::configs.max_mov + 1;
+	moviment = rand() % max * pow(-1, rand() % 2);
 	y_position += moviment;
 
-	if(!(y_position > size_y + 1 && y_position < max_y)){
-		if(io::configs.no_bounded)
+	if (!(y_position > size_y + 1 && y_position < max_y)) {
+		if (io::configs.no_bounded)
 			this->dead = true;
 		else
-			y_position += -2*moviment;
+			y_position += -2 * moviment;
 	}
 
 	this->y_position = y_position;
 }
 
-void Tracker::recreate(){
+void Tracker::recreate() {
 	this->id = to_id++;
 
-	double recreate = (rand()%100)/100;
-	if(io::configs.recreate_chance < recreate){
+	double recreate = (rand() % 100) / 100;
+	if (io::configs.recreate_chance < recreate) {
 		this->frames = frames;
 
 		this->max_x_pos = frames.cols;
@@ -175,15 +174,17 @@ void Tracker::recreate(){
 		this->dead = false;
 	}
 }
-void Tracker::setPosition(){
-	uint max_x = this->max_x_pos - 2*(this->size_x + 1);
-	uint max_y = this->max_y_pos - 2*(this->size_y + 1);
+void Tracker::setPosition() {
+	uint max_x = this->max_x_pos - 2 * (this->size_x + 1);
+	uint max_y = this->max_y_pos - 2 * (this->size_y + 1);
 
-	this->x_position = rand()%max_x + this->size_x + 1;
-	this->y_position = rand()%max_y + this->size_x + 1;
+	this->x_position = rand() % max_x + this->size_x + 1;
+	this->y_position = rand() % max_y + this->size_x + 1;
+
+	double x = rand() % 10000 / 10000;
 }
 
-Tracker::Tracker(Mat frames){
+Tracker::Tracker(Mat frames) {
 	this->id = to_id++;
 	this->dead = false;
 
@@ -200,42 +201,34 @@ Tracker::Tracker(Mat frames){
 	this->createMat();
 }
 
-void Tracker::createMat(){
+void Tracker::createMat() {
 	this->image = Mat::zeros(this->size_x, this->size_y, CV_8UC3);
 
-	circle(this->image,
-		Point(floor(this->size_x/2), floor(this->size_y/2)),
-		floor(this->size_x/2) - 1,
-		Scalar( red, blue, green),
-		-1,
-		8);
+	circle(this->image, Point(floor(this->size_x / 2), floor(this->size_y / 2)),
+		   floor(this->size_x / 2) - 1, Scalar(red, blue, green), -1, 8);
 
-	for(uint i = 0; i < io::configs.num_features; i++){
-		uint size_total = floor(this->size_x/2) - 1;
-		uint size_parcial = ceil(this->size_x/4);
-		uint max  = floor(size_total - size_parcial) - 1;
-		//uint max = 1;
-		int x_variation = (rand()%max) * pow(-1, rand()%2);
-		int y_variation = (rand()%max) * pow(-1, rand()%2);
+	for (uint i = 0; i < io::configs.num_features; i++) {
+		uint size_total = floor(this->size_x / 2) - 1;
+		uint size_parcial = ceil(this->size_x / 4);
+		uint max = floor(size_total - size_parcial) - 1;
+		// uint max = 1;
+		int x_variation = (rand() % max) * pow(-1, rand() % 2);
+		int y_variation = (rand() % max) * pow(-1, rand() % 2);
 
-		Scalar color =  this->randomColor();
+		Scalar color = this->randomColor();
 
-		circle(this->image,
-			Point(floor(this->size_x/2) + x_variation, floor(this->size_y/2) + y_variation),
-			ceil(this->size_x/4),
-			color,
-			-1,
-			8);
+		circle(this->image, Point(floor(this->size_x / 2) + x_variation,
+								  floor(this->size_y / 2) + y_variation),
+			   ceil(this->size_x / 4), color, -1, 8);
 	}
-
 }
 
-Scalar Tracker::randomColor(){
-	return Scalar(rand()%256, rand()%256, rand()%256);
+Scalar Tracker::randomColor() {
+	return Scalar(rand() % 256, rand() % 256, rand() % 256);
 }
 
-void Tracker::createColors(){
-	this->red = rand()%128 + 128;
-	this->blue = rand()%128 + 128;
-	this->green = rand()%128 + 128;
+void Tracker::createColors() {
+	this->red = rand() % 128 + 128;
+	this->blue = rand() % 128 + 128;
+	this->green = rand() % 128 + 128;
 }
